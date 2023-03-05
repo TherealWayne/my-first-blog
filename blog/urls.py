a5962ad968d0d98c1f7e39fd58fd30c5f7b08a4c
Wayne Bell <117953075+TherealWayne@users.noreply.github.com>
2023-03-05 12:12:00 -0500

    Add files via upload

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,22 @@
-from django.urls import path
-from . import views 
+from django.urls import path, include
+from django.contrib import admin
 
+from django.contrib.auth import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-] 
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('', include('blog.urls')),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+]
 
